672a75d85c4ceeb24c84458433aa7159599f7bf1
Glasswinner <amoghsubramanian56@gmail.com>
2025-08-06 22:12:41 +0000

    chore: configure Parcel alias for process via .parcelrc

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-# Build all HTML files, polyfilling `process`
-npx parcel build *.html \
-  --dist-dir dist \
-  --public-url ./ \
-  --alias process=process/browser
+npx parcel build *.html --dist-dir dist --public-url ./
